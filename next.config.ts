762c04c09a543e39edd330dce35f8f4e20645f31
Anonymous <anonymous@example.com>
2025-02-15 15:24:36 +0100

    first commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,42 @@
-import type { NextConfig } from "next";
+import { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: 'https://fc-backend-cnxm.onrender.com' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET,DELETE,PATCH,POST,PUT' },
+          { key: 'Access-Control-Allow-Headers', value: 'Accept, Accept-Version, Content-Length, Content-Type, Date' },
+        ]
+      }
+    ]
+  },
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: 'forbes-partners.com', pathname: '/wp-content/uploads/**' },
+      { protocol: 'https', hostname: 'images.ctfassets.net', pathname: '/qtbqvna1l0yq/**' }
+    ],
+    dangerouslyAllowSVG: true,
+    contentDispositionType: 'attachment',
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+    domains: ['127.0.0.1', 'localhost', 'fc-backend-cnxm.onrender.com'],
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: process.env.NEXT_PUBLIC_API_URL
+          ? `${process.env.NEXT_PUBLIC_API_URL}/:path*`
+          : '/api/:path*'
+      }
+    ]
+  }
+}
 
-export default nextConfig;
+export default nextConfig
