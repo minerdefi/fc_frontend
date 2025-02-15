aa428a9fdbd6dc68badbe713d8e4eca79fa29de6
minerdefi <snick4714@gmail.com>
2025-02-15 11:29:22 +0100

    first commit

--- src/components/dashboard/DashboardSidebar.tsx
+++ src/components/dashboard/DashboardSidebar.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useAuth } from '@/context/AuthContext';
+import { useAuth } from '../../context/AuthContext';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
     faHome,
