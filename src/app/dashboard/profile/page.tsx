91956e6773b4276cf1e671e745c2cb2331a2a47d
Anonymous <anonymous@example.com>
2025-02-15 10:43:40 +0100

    first commit

--- src/app/dashboard/profile/page.tsx
+++ src/app/dashboard/profile/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useAuth } from '@/context/AuthContext';
+import { useAuth } from '../../../context/AuthContext';
 import { useState } from 'react';
 import Link from 'next/link';
 
